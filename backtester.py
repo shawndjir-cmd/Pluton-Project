7fccfe94a973d92a912a16736bef6ffe1fc65f14
shawn_djirosse <shawndjir@gmail.com>
2026-07-26 22:14:57 -0400

    Create backtester.py

---
+++ backtester.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+from portfolio import Portfolio
+
+
+class Backtester:
+    
+    #Runs trading strategy on historical data
+    def __init__(self, strategy, initial_cash=10000, take_profit=0.10, stop_loss=0.05): 
+        
+        self.strategy = strategy
+        self.portfolio = Portfolio(initial_cash=initial_cash, take_profit=take_profit, stop_loss=stop_loss)
+
+    def run(self, data):
+
+        # Generate BUY and SELL signals
+        signals = self.strategy.generate_signals(data)
+
+        portfolio_values = []
+
+        # Loop through each trading day
+        for _, row in signals.iterrows():
+
+            price = row["Close"]
+            signal = row["Signal"]
+
+            # ----------------------------------
+            # Check Take Profit / Stop Loss
+            # ----------------------------------
+
+            if self.portfolio.should_sell(price):
+                self.portfolio.sell(price)
+
+            # ----------------------------------
+            # Execute SMA Signals
+            # ----------------------------------
+
+            if signal == 1:
+                self.portfolio.buy(price)
+
+            elif signal == -1:
+                self.portfolio.sell(price)
+
+            # ----------------------------------
+            # Record Portfolio Value
+            # ----------------------------------
+
+            value = self.portfolio.value(price)
+
+            portfolio_values.append(value)
+
+        signals["Portfolio Value"] = portfolio_values
+
+        return signals
