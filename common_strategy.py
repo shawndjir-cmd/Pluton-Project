a4e621017f8142db7b0681b8773534aabacc05ba
shawn_djirosse <shawndjir@gmail.com>
2026-07-26 22:10:32 -0400

    Update common_strategy.py

--- common_strategy.py
+++ common_strategy.py
@@ -1,24 +1,11 @@
 from abc import ABC, abstractmethod
 
 
-class BaseStrategy(ABC):
-    """
-    Basic class for all the different trading strategies.
-    """
-
+class BaseStrategy(ABC):  
+    #Basic class for all the different trading strategies.
+    
     @abstractmethod
+    
     def generate_signals(self, data):
-        """
-        Finds trading signals.
-
-        Parameters
-        ----------
-        data : pandas.DataFrame
-            Historical market data.
-
-        Returns
-        -------
-        pandas.DataFrame
-            DataFrame containing trading signals.
-        """
+        #To check which strategy to use like RSI, SMA or MACD   
         pass
